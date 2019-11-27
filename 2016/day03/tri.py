2b9dde8912fdc72a0fa3fb7ad5ec65dc71f981ce
Robert Musser <robert.musser@questu.ca>
2019-11-27 10:14:15 -0800

    fixed it so abc tri where a+b=c is not read as a tri anymore

--- 2016/day03/tri.py
+++ 2016/day03/tri.py
@@ -1,9 +1,9 @@
 def tricheck(a, b, c):
-    if a+b < c:
+    if a+b <= c:
         return 0
-    if b+c < a:
+    if b+c <= a:
         return 0
-    if c+a < b:
+    if c+a <= b:
         return 0
     return 1
 
@@ -11,6 +11,22 @@ def tricheck(a, b, c):
 # main program
 print(tricheck(5, 5, 5))
 print(tricheck(1, 50, 2))
+
+index = []
+with open('input.txt') as fp:
+    line = fp.readline()
+
+
+filepath = 'Iliad.txt'
+with open(filepath) as fp:
+   line = fp.readline()
+   cnt = 1
+   while line:
+       print("Line {}: {}".format(cnt, line.strip()))
+       line = fp.readline()
+       cnt += 1
+
+
 
 
 
