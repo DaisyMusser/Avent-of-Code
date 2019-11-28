4ca2f038a198c2cecd54d37073f2aaeb2b6692b3
Robert Musser <robert.musser@questu.ca>
2019-11-27 17:06:49 -0800

    py: pt2 solved! s/d clean up code tho

--- 2015/day01/pt2.py
+++ 2015/day01/pt2.py
@@ -1,19 +1,27 @@
-# takes direction list, returns specified floor
-def translator(directions):
+# takes direction list, returns record list of specified floors
+def tracker(directions):
     flr = 0
+    record = []
     for i in range(len(directions)):
         if directions[i] == '(':
             flr += 1
         if directions[i] == ')':
             flr -= 1
-    return flr
+        record.append(flr)
+    return record
+
+
+# returns loc of first instance of -1 in the record
+def basement(record):
+    x = record.index(-1)
+    return x+1
 
 
 # main program?
 file = 'input.txt'      # This is all you have to change to deal with dif. input file names
 with open(file) as fp:
     directions = fp.readline()
-    print(directions)
-    properFloor = translator(directions)
-    print(properFloor)
+    record = tracker(directions)
+    loc = basement(record)
+    print(loc)
 
