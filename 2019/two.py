7dfaa9a4a8014db3fc8838d1c523f7a03a03c081
Robert Musser <robert.musser@questu.ca>
2019-12-01 10:47:02 -0800

    py: one works, started on two

--- 2019/two.py
+++ 2019/two.py
@@ -3,7 +3,10 @@ import math
 
 def fuel_counter(mass):
     # devide by 3 round down then subtract 2
-    return (math.floor(mass/3))-2
+    fuel = (math.floor(mass/3))-2
+    if fuel < 1:
+        fuel = 0
+    return fuel
 
 
 # Reads file into modules array, code adapted from ( https://github.com/imhoffman/advent/blob/master/2015/01/one.py )
@@ -26,15 +29,17 @@ def module_tidier(messy_modules):
         tidy_modules[i] = int(tidy_modules[i])
     return tidy_modules
 
-
+def recursive_fuel_calculator(fuel):
+    total
 # main function
 messy_modules = file_reader('input.txt')  # change here for different file_names
 tidy_modules = module_tidier(messy_modules)
 
-total_fuel = 0
+total_modu_fuel = 0
 
 for i in range(len(tidy_modules)):
     total_fuel += (fuel_counter(tidy_modules[i]))
 
-print(total_fuel)
+
+
 
