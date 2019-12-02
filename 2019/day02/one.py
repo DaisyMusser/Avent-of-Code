304dcfbfc20e70b3ca309096d090f8de7542504e
Robert Musser <robert.musser@questu.ca>
2019-12-02 10:22:41 -0800

    py: indexed all commas in string sucessfully

--- 2019/day02/one.py
+++ 2019/day02/one.py
@@ -1,5 +1,5 @@
-# Reads file into modules array, code adapted from ( https://github.com/imhoffman/advent/blob/master/2015/01/one.py )
-def file_reader(file_name):
+# Reads file into string, code adapted from ( https://github.com/imhoffman/advent/blob/master/2015/01/one.py )
+def file_to_string(file_name):
     with open(file_name) as fp:
         while True:
             line = fp.read()
@@ -9,6 +9,21 @@ def file_reader(file_name):
     return string_opcode
 
 
+# parses string into an array
+def comma_finder(string_opcode):
+    start_search = 0
+    all_commas = []
+    while True:
+        comma_loc = string_opcode.find(',', start_search)
+        if comma_loc == -1:
+            break
+        all_commas.append(comma_loc)
+        start_search = comma_loc + 1
+    return all_commas
+
+
 # main program:
-x = file_reader('input.txt')
-print(x)
+string_opcode = file_to_string('input.txt')    # change here for differnt file names
+all_commas = comma_finder(string_opcode)
+print(string_opcode)
+print(all_commas)
