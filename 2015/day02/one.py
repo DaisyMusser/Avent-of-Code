77e58fc4fcbb3881d857ea268708ee973b091ef2
Robert Musser <robert.musser@questu.ca>
2019-12-02 21:14:47 -0800

    py: getting an answer but its too high

--- 2015/day02/one.py
+++ 2015/day02/one.py
@@ -1,15 +1,52 @@
-# 
+import sys
+
+# reads file into present array
 def file_reader(file_name):
     presents = []
     with open(file_name) as fp:
         while True:
-            line = fp.readline()
+            line = fp.read()
             if not line:
                 break
-            presents.append(line)
+            presents = line
     return presents
 
 
+# converts str to array
+def list_formatter(presents):
+    presents = presents.replace('x', ' ')
+    presents = presents.replace('\n', ' ')
+    presents = presents.split(' ')
+    return presents
+
+
+def paper_calculator(presents):
+    total_paper = 0
+    box_start = 0
+    for i in range(1000):
+        l = int(presents[box_start])
+        w = int(presents[box_start+1])
+        h = int(presents[box_start+2])
+        total_paper += 2*l*w + 2*w*h + 2*h*l   # surface area of box
+        side_one = h*l
+        side_two = l*w
+        side_three = w*h
+        if side_one <= side_two and side_one <= side_three:
+            total_paper += side_one
+        elif side_two <= side_one and side_two <= side_three:
+            total_paper += side_two
+        elif side_three <= side_one and side_three <= side_two:
+            total_paper += side_three
+    return total_paper
+
+
 # main program
-presents = file_reader("input.txt")
-print(presents)
+presents = file_reader('input.txt')     # change here for different file names
+presents = list_formatter(presents)
+# done with file formatting
+
+# on to file processing
+total = paper_calculator(presents)
+print(total)
+
+
