6ab60d4bc226fb3a0272b9fefac69afd90f3b6ab
Robert Musser <robert.musser@questu.ca>
2019-12-03 09:53:03 -0800

    c: stole some more IO code from ibid

--- 2019/day03/one.c
+++ 2019/day03/one.c
@@ -29,5 +29,30 @@ void reader ( FILE* f, int* maxlen, char lines[][MAX_LINE_LENGTH] ) {
 
 
 int main ( void ) {
+ //adapted from: https://github.com/imhoffman/advent/blob/master/2019/01/one.c
+ FILE* fp;
+ int number_of_lines, longest_line, total;
+ char (*temp)[MAX_LINE_LENGTH]
+	 = malloc( MAX_LINES_IN_FILE * sizeof( char ) * MAX_LINE_LENGTH );
+         //  do we need to cast from (void *) to (char *) ?
+
+ //  file I/O and memory allocation of memory input
+ fp = fopen("puzzle.txt","r");
+ reader( fp, &number_of_lines, &longest_line, temp );  // n and longest_line are known upon return
+ fclose(fp);        // done with file...only read the file once
+
+ printf( "\n read %d lines from the input file\n", number_of_lines );
+ printf( "\n the longest line is %d chars long\n\n", longest_line );
+
+ // declare an array of appropriate length and width
+ char input[number_of_lines][longest_line+1];
+
+ //  populate `input` with puzzle data for use going forward
+ for( int i=0; i < number_of_lines; i++ ) {
+  strncpy( input[i], temp[i], longest_line+1 );
+ }
+ // `input` now has the data...free the big temp array from the file read
+ free( temp );
+ //  end of file I/O
 
 }
