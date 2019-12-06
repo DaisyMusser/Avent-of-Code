c03b6d1f495788a1f343be67e5a3720e148df4af
Robert Musser <robert.musser@questu.ca>
2019-12-05 16:18:13 -0800

    getting a seg fault on running

--- 2019/day01/one.c
+++ 2019/day01/one.c
@@ -7,11 +7,14 @@
 #define MAX_LINES_IN_FILE     30000
 
 // should return an array of that contains only the input
-char reader ( int *max_lenght, int *number_of_lines, char file_name[MAX_LINE_LENGTH] ) {
+void reader ( int *max_lenght, int *number_of_lines, char file_name[MAX_LINE_LENGTH], char temp[][MAX_LINE_LENGTH] ){
     FILE* fp;
     char buffer[MAX_LINE_LENGTH];   // fgets null-terminates
-    char *qnull, (*temp)[MAX_LINE_LENGTH] = malloc( MAX_LINES_IN_FILE * sizeof( char ) * MAX_LINE_LENGTH );
+    char *qnull;
     size_t maxtemp;
+    
+    *number_of_lines = 0;
+    *max_lenght = 0;
 
     fp = fopen( file_name, "r" );
     while ( fgets(buffer, MAX_LINE_LENGTH, fp) != NULL ) {
@@ -19,21 +22,14 @@ char reader ( int *max_lenght, int *number_of_lines, char file_name[MAX_LINE_LEN
         qnull = strchr( buffer, '\0' );
         maxtemp = qnull - &buffer[0];
         if ( (int)maxtemp > *max_lenght ) {
-            max_lenght = (int)maxtemp;
+            *max_lenght = (int)maxtemp;
         }
-        *number_of_lines ++;
+        *number_of_lines ++;     // gives me an expression unused warning on compile? dont know why??
     }
     
     fclose( fp );
 
-    char input[number_of_lines][max_lenght+1];
-    for( int i=0; i < number_of_lines; i++ ) {
-        strncpy( input[i], temp[i], max_lenght+1 );
-    }
-    
-    free( temp );
-
-    return &input;
+    return;
 }
 
 
@@ -51,11 +47,19 @@ int fuel_parser ( const int mass ) {
 // main program
 //
 int main ( void ) {
-    char file_name[MAX_LINE_LENGTH] = "input.txt";
-    int max_lenght = 0;
-    int number_of_lines = 0;
+    char file_name[MAX_LINE_LENGTH] = "input.txt";     // change file name here!
+    int max_lenght;
+    int number_of_lines;
+    char (*temp)[MAX_LINE_LENGTH] = malloc( MAX_LINES_IN_FILE * sizeof( char ) * MAX_LINE_LENGTH );
     
-    input = reader( &max_lenght, &number_of_lines, file_name );
+    reader( &max_lenght, &number_of_lines, file_name, temp );
+    
+    char input[number_of_lines][max_lenght+1];
+    for( int i=0; i < number_of_lines; i++ ) {
+        strncpy( input[i], temp[i], max_lenght+1 );
+    }
+    
+    free( temp );
     
     return 0;
 }
