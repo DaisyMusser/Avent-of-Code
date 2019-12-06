bf7c784a3669a21f585dc2518a31f61f13ad28af
Robert Musser <robert.musser@questu.ca>
2019-12-05 22:05:49 -0800

    done with file io, not sure now to continue

--- 2019/06/one.py
+++ 2019/06/one.py
@@ -6,13 +6,27 @@ def file_reader(file_name):
             line = file_pointer.readline()
             if not line:
                 break
-            if len(line) == 9:
-                orbit_map.append(line[0:8])
-    # objects = orbit_map.split(')')
-    return orbit_map#, objects
+            orbit_map.append(line[0:8])
+    return orbit_map
+
+
+def orbit_sorter(old_orbit_map):
+    objects = set()
+    orbit_map = []
+    for orbit in old_orbit_map:          # removes newline from each item of list
+        if orbit != '\n':                # removes newline item from end of list
+            orbit_map.append(orbit.replace('\n', ''))
+
+    for orbit in orbit_map:
+        x = orbit.split(')')
+        for i in range(2):
+            objects.add(x[i])
+
+    return orbit_map, objects
 
 
 # main program
 orbit_map = file_reader('input.txt')      # change filename here!
+orbit_map, objects = orbit_sorter(orbit_map)
 
-print(orbit_map)
+print(len(objects)-1)
