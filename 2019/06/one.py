d99c3e120d218a51fc4c76ff982e5008f4c6c8e8
Robert Musser <robert.musser@questu.ca>
2019-12-06 09:41:18 -0800

    polished up

--- 2019/06/one.py
+++ 2019/06/one.py
@@ -1,4 +1,5 @@
 # from https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-python-programs-execution
+# might try to decrease runtime in future, runtime reporting helpful for that
 import time
 start_time = time.time()
 
@@ -10,15 +11,15 @@ def file_reader(file_name):
             line = file_pointer.readline()
             if not line:
                 break
-            orbit_map.append(line[0:8])
+            orbit_map.append(line[0:8])   # not all of line bc we don't want '\n'
     return orbit_map
 
 
 # parses orbit_map into a more workable orbit_map and the set of all objects
 def orbit_sorter(old_orbit_map):
-    objects = set()
+    objects = set()                      # set to remove duplicates and be fancy
     orbit_map = []
-    for orbit in old_orbit_map:          # removes newline from each item of list
+    for orbit in old_orbit_map:
         if orbit != '\n':                # removes newline item from end of list
             orbit_map.append(orbit.replace('\n', ''))
 
@@ -32,11 +33,9 @@ def orbit_sorter(old_orbit_map):
 
 # given the base orbit it counts its way up and returns the number of orbits
 def orbit_counter_upper(object, orbit_map):
-    # print('   into orbit_counter_upper')
     counter = 0
     one_up = object
     while base_search(one_up, orbit_map) != -1:
-        # print('      into while-base orbit exists loop')
         x = base_search(one_up, orbit_map)
         one_up = x[0:3]
         counter += 1
@@ -45,9 +44,7 @@ def orbit_counter_upper(object, orbit_map):
 
 # returns base orbit of an object
 def base_search(object, orbit_map):
-    # print('         into base_search')
     for orbit in orbit_map:
-        # print('            into for-orbits search loop')
         if orbit[4:7] == object:
             return orbit
     return -1
@@ -58,13 +55,12 @@ orbit_map = file_reader('input.txt')      # change filename here!
 orbit_map, objects = orbit_sorter(orbit_map)
 objects.remove('COM')
 
-# print("Passed file IO")
-
 total_orbits = 0
 for object in objects:
     # print("Into all-objects loop (in main)")
     total_orbits += orbit_counter_upper(object, orbit_map)
 
 print('Answer: ', total_orbits)
-# see top for time source
+# see top for time source, mac runtime <80s 
 print("--- %s seconds ---" % (time.time() - start_time))
+
