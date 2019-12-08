0afded14ece8f8e6e5531e71e6578140a43b4dda
Robert Musser <robert.musser@questu.ca>
2019-12-08 12:23:03 -0800

    start part two

---
+++ 2019/08/two.py
@@ -0,0 +1,31 @@
+# dumps orbits into list
+def file_reader(file_name):
+    with open(file_name) as file_pointer:
+        while True:
+            line = file_pointer.read()
+            if not line:
+                break
+            raw_data = line[0:(len(line)-2)]    # strips \n\n from end of line
+    return raw_data
+
+
+# stealing populating method from: https://www.ict.social/python/basics/multidimensional-lists-in-python
+def data_processor(raw_data):
+    counter = 0
+    clean_data = []
+    while True:
+        layer = []
+        for _ in range(6):
+            line = []
+            for _ in range(25):
+                if counter == len(raw_data):
+                    return clean_data
+                line.append(int(raw_data[counter]))
+                counter += 1
+            layer.append(line)
+        clean_data.append(layer)
+
+
+# main program
+data = file_reader('input.txt')  # change file name here!
+data = data_processor(data)
