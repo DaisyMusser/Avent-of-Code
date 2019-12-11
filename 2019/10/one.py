d907518865162e01ec9e6160a9126bd33198bf27
Robert Musser <robert.musser@questu.ca>
2019-12-10 20:54:14 -0800

    class __init__ works

--- 2019/10/one.py
+++ 2019/10/one.py
@@ -15,13 +15,13 @@ def asteroid_finder(raw_map):
     for i in range(len(raw_map)):
         for ii in range(len(raw_map[i])):
             if raw_map[i][ii] == '#':
-                xy = (ii, i)                  # xy's are tuples
-                asteroid_xy.add(xy)
+                xy = (ii, -i)                  # xy's are tuples
+                asteroid_xy.add(xy)           # asteroid_xy is a set
     return asteroid_xy
 
 
 # main program one: file io and makes asteroid_xy
-raw_map = file_reader('input.txt')            # change file name here
+raw_map = file_reader('test.txt')            # change file name here
 asteroid_xy = asteroid_finder(raw_map)
 
 
@@ -30,5 +30,23 @@ class Asteroid:
         self.xy = xy
         self.x = xy[0]
         self.y = xy[1]
-        self.map = asteroid_xy
+        self.absolute_map = asteroid_xy         # currently includes the spec asteroid object
+        relative_map = []
+        for asteroid in self.absolute_map:
+            rel_x = asteroid[0] + (self.x * -1)
+            rel_y = asteroid[1] + (self.y * -1)
+            relative_xy = (rel_x, rel_y)        # relative xy's are tuples
+            relative_map.append(relative_xy)
+        self.relative_map = relative_map
         return
+
+    def look_for_asteroids(self):
+        asteroids_found = set(self.relative_map[:])
+        asteroids_found.remove((0, 0))
+        
+        return asteroids_found
+
+
+# main program two:
+test = Asteroid((1, 0))
+test.look_for_asteroids()
