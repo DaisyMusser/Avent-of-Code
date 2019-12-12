94f18dad206d6b87442f58c66ef59e04c8974f2d
Robert Musser <robert.musser@questu.ca>
2019-12-12 11:53:12 -0800

    total energy method was missing abs() and velocities are NOT reset, both fixed now

--- 2019/12/one.py
+++ 2019/12/one.py
@@ -1,4 +1,6 @@
 # day 12: tracking the moons
+import math
+
 def file_to_string(file_name):
     locations = []
     with open(file_name) as fp:
@@ -57,7 +59,7 @@ class JupMoon(object):
         self.x = loc[0]
         self.y = loc[1]
         self.z = loc[2]
-        self.velocity = [0, 0, 0]  # does velocity need to be reset every time step?
+        self.velocity = [0, 0, 0]  # does velocity need to be reset every time step? edit: no
         return
 
     def apply_gravity(self, moon_one, moon_two, moon_three):   # pass in loc of other three moons
@@ -81,12 +83,12 @@ class JupMoon(object):
     def apply_velocity(self):
         for i in range(3):
             self.loc[i] += self.velocity[i]
-        self.velocity = [0, 0, 0]      # resets velocity for next gav calc
+        # self.velocity = [0, 0, 0]      # resets velocity for next gav calc
         return
 
     def calc_total_energy(self):
-        kinetic = self.loc[0] + self.loc[1] + self.loc[2]
-        potential = self.velocity[0] + self.velocity[1] + self.velocity[2]
+        kinetic = abs(self.loc[0]) + abs(self.loc[1]) + abs(self.loc[2])
+        potential = abs(self.velocity[0]) + abs(self.velocity[1]) + abs(self.velocity[2])
         return kinetic + potential
 
 
@@ -96,38 +98,41 @@ def run_one(time_steps, locations):
     europa = JupMoon(locations[1])
     ganymede = JupMoon(locations[2])
     callisto = JupMoon(locations[3])
-    print('After 0 steps:\npos=<', io.x, ', ', io.y, ', ', io.z, '>, ', 'vel=<', io.velocity[0], end='')
-    print(', ', io.velocity[1], ', ', io.velocity[2], '>')
-    print('pos = < ', europa.x, ', ', europa.y, ', ', europa.z, ' >, ', 'vel = < ', europa.velocity[0], end='')
-    print(', ', europa.velocity[1], ', ', europa.velocity[2], '>')
-    print('pos = < ', ganymede.x, ', ', ganymede.y, ', ', ganymede.z, ' >, ', 'vel = < ', ganymede.velocity[0], end='')
-    print(', ', ganymede.velocity[1], ', ', ganymede.velocity[2], '>')
-    print('pos = < ', callisto.x, ', ', callisto.y, ', ', callisto.z, ' >, ', 'vel = < ', callisto.velocity[0], end='')
-    print(', ', callisto.velocity[1], ', ', callisto.velocity[2], '>')
+    # print('After 0 steps:\npos=<', io.x, ', ', io.y, ', ', io.z, '>, ', 'vel=<', io.velocity[0], end='')
+    # print(', ', io.velocity[1], ', ', io.velocity[2], '>')
+    # print('pos = < ', europa.x, ', ', europa.y, ', ', europa.z, ' >, ', 'vel = < ', europa.velocity[0], end='')
+    # print(', ', europa.velocity[1], ', ', europa.velocity[2], '>')
+    # print('pos = < ', ganymede.x, ', ', ganymede.y, ', ', ganymede.z, ' >, ', 'vel = < ', ganymede.velocity[0], end='')
+    # print(', ', ganymede.velocity[1], ', ', ganymede.velocity[2], '>')
+    # print('pos = < ', callisto.x, ', ', callisto.y, ', ', callisto.z, ' >, ', 'vel = < ', callisto.velocity[0], end='')
+    # print(', ', callisto.velocity[1], ', ', callisto.velocity[2], '>')
     for step in range(time_steps):
-
+        print('step: ', step)
+        print('  io velocity: ', io.velocity)
         io.apply_gravity(europa.loc, ganymede.loc, callisto.loc)
         europa.apply_gravity(io.loc, ganymede.loc, callisto.loc)
         ganymede.apply_gravity(io.loc, europa.loc, callisto.loc)
         callisto.apply_gravity(io.loc, europa.loc, ganymede.loc)
+        print('    new io velocity: ', io.velocity)
 
+        print('      io loc: ', io.loc)
         io.apply_velocity()
         europa.apply_velocity()
         ganymede.apply_velocity()
         callisto.apply_velocity()
+        print('        new io loc: ', io.loc)
 
-        print('After ', step, ' steps:\npos=<', io.x, ', ', io.y, ', ', io.z, '>, ', 'vel=<', io.velocity[0], end='')
-        print(', ', io.velocity[1], ', ', io.velocity[2], '>')
-        print('pos=<', europa.x, ', ', europa.y, ', ', europa.z, '>, ', 'vel=<', europa.velocity[0], end='')
-        print(', ', europa.velocity[1], ', ', europa.velocity[2], '>')
-        print('pos=<', ganymede.x, ', ', ganymede.y, ', ', ganymede.z, '>, ', 'vel=<', ganymede.velocity[0],
-              end='')
-        print(', ', ganymede.velocity[1], ', ', ganymede.velocity[2], '>')
-        print('pos=<', callisto.x, ', ', callisto.y, ', ', callisto.z, '>, ', 'vel=<', callisto.velocity[0],
-              end='')
-        print(', ', callisto.velocity[1], ', ', callisto.velocity[2], '>')
-
-    total_total_energy = (io.calc_total_energy() + europa.calc_total_energy() + ganymede.calc_total_energy() + callisto.calc_total_energy())
+        # print('After ', step, ' steps:\npos=<', io.x, ', ', io.y, ', ', io.z, '>, ', 'vel=<', io.velocity[0], end='')
+        # print(', ', io.velocity[1], ', ', io.velocity[2], '>')
+        # print('pos=<', europa.x, ', ', europa.y, ', ', europa.z, '>, ', 'vel=<', europa.velocity[0], end='')
+        # print(', ', europa.velocity[1], ', ', europa.velocity[2], '>')
+        # print('pos=<', ganymede.x, ', ', ganymede.y, ', ', ganymede.z, '>, ', 'vel=<', ganymede.velocity[0],
+        #      end='')
+        # print(', ', ganymede.velocity[1], ', ', ganymede.velocity[2], '>')
+        # print('pos=<', callisto.x, ', ', callisto.y, ', ', callisto.z, '>, ', 'vel=<', callisto.velocity[0],
+        #       end='')
+        # print(', ', callisto.velocity[1], ', ', callisto.velocity[2], '>')
+    total_total_energy = io.calc_total_energy() + europa.calc_total_energy() + ganymede.calc_total_energy() + callisto.calc_total_energy()
     return total_total_energy
 
 
