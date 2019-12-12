2da428087d9f36d1d69f987099437df0fe96df2d
Robert Musser <robert.musser@questu.ca>
2019-12-12 11:55:58 -0800

    works on example one but not giving the right answer

--- 2019/12/one.py
+++ 2019/12/one.py
@@ -83,7 +83,6 @@ class JupMoon(object):
     def apply_velocity(self):
         for i in range(3):
             self.loc[i] += self.velocity[i]
-        # self.velocity = [0, 0, 0]      # resets velocity for next gav calc
         return
 
     def calc_total_energy(self):
@@ -98,14 +97,7 @@ def run_one(time_steps, locations):
     europa = JupMoon(locations[1])
     ganymede = JupMoon(locations[2])
     callisto = JupMoon(locations[3])
-    # print('After 0 steps:\npos=<', io.x, ', ', io.y, ', ', io.z, '>, ', 'vel=<', io.velocity[0], end='')
-    # print(', ', io.velocity[1], ', ', io.velocity[2], '>')
-    # print('pos = < ', europa.x, ', ', europa.y, ', ', europa.z, ' >, ', 'vel = < ', europa.velocity[0], end='')
-    # print(', ', europa.velocity[1], ', ', europa.velocity[2], '>')
-    # print('pos = < ', ganymede.x, ', ', ganymede.y, ', ', ganymede.z, ' >, ', 'vel = < ', ganymede.velocity[0], end='')
-    # print(', ', ganymede.velocity[1], ', ', ganymede.velocity[2], '>')
-    # print('pos = < ', callisto.x, ', ', callisto.y, ', ', callisto.z, ' >, ', 'vel = < ', callisto.velocity[0], end='')
-    # print(', ', callisto.velocity[1], ', ', callisto.velocity[2], '>')
+
     for step in range(time_steps):
         print('step: ', step)
         print('  io velocity: ', io.velocity)
@@ -122,16 +114,6 @@ def run_one(time_steps, locations):
         callisto.apply_velocity()
         print('        new io loc: ', io.loc)
 
-        # print('After ', step, ' steps:\npos=<', io.x, ', ', io.y, ', ', io.z, '>, ', 'vel=<', io.velocity[0], end='')
-        # print(', ', io.velocity[1], ', ', io.velocity[2], '>')
-        # print('pos=<', europa.x, ', ', europa.y, ', ', europa.z, '>, ', 'vel=<', europa.velocity[0], end='')
-        # print(', ', europa.velocity[1], ', ', europa.velocity[2], '>')
-        # print('pos=<', ganymede.x, ', ', ganymede.y, ', ', ganymede.z, '>, ', 'vel=<', ganymede.velocity[0],
-        #      end='')
-        # print(', ', ganymede.velocity[1], ', ', ganymede.velocity[2], '>')
-        # print('pos=<', callisto.x, ', ', callisto.y, ', ', callisto.z, '>, ', 'vel=<', callisto.velocity[0],
-        #       end='')
-        # print(', ', callisto.velocity[1], ', ', callisto.velocity[2], '>')
     total_total_energy = io.calc_total_energy() + europa.calc_total_energy() + ganymede.calc_total_energy() + callisto.calc_total_energy()
     return total_total_energy
 
@@ -140,7 +122,7 @@ def run_one(time_steps, locations):
 locations = file_to_string('ten_steps.txt')    # change file name here
 locations = formatter(locations)
 
-answer = run_one(10, locations)                # change steps here
+answer = run_one(100, locations)                # change steps here
 print(answer)
 
 
