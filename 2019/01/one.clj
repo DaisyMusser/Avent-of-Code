0a5ed1c1487f557562fccf227160a4e0408c8711
Robert Musser <robert.musser@questu.ca>
2019-12-17 15:44:40 -0800

    started clojure, stole from https://stackoverflow.com/questions/28551000/what-is-the-best-way-to-round-numbers-in-clojure/28551232

---
+++ 2019/01/one.clj
@@ -0,0 +1,6 @@
+
+(defn fuel_checker [mass] (- (Math/floor (/ mass 3)) 2) ) 
+
+(int (Math/floor (/ 3 2)))
+;https://stackoverflow.com/questions/28551000/what-is-the-best-way-to-round-numbers-in-clojure/28551232
+
