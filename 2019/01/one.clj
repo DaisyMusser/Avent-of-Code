c6d495b099c74abde7bff720fc2474ff0582685a
Robert Musser <robert.musser@questu.ca>
2019-12-17 20:03:11 -0800

    added some code from https://gitlab.com/imhoffman/fa19b4-mat3006/raw/master/code/day17/2019_01_one.clj

--- 2019/01/one.clj
+++ 2019/01/one.clj
@@ -1,6 +1,26 @@
+;; calculates fuel for a mass
+(defn fuel-counter [mass] (int (- (Math/floor (/ mass 3)) 2)))
 
-(defn fuel_checker [mass] (- (Math/floor (/ mass 3)) 2) ) 
 
-(int (Math/floor (/ 3 2)))
-;https://stackoverflow.com/questions/28551000/what-is-the-best-way-to-round-numbers-in-clojure/28551232
+;; total fuel calc
+;; https://gitlab.com/imhoffman/fa19b4-mat3006/raw/master/code/day17/2019_01_one.clj
+(defn total-fuel [list-of-masses accum]
+  (if (empty? list-of-masses)
+    accum
+    (recur (rest list-of-masses) (+ accum (fuel-counter (first (int list-of-masses)))))))
+
+
+;; main program
+
+;; input is the contents of input.txt
+(def input
+  (with-open [f (clojure.java.io/reader "input.txt")]
+    (reduce conj () (line-seq f))))      
+
+;; answer
+;; https://gitlab.com/imhoffman/fa19b4-mat3006/raw/master/code/day17/2019_01_one.clj
+(println
+  " part one: the fuel needed for the module masses is"
+  (total-fuel input 0))
+
 
