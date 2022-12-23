c17ae997a85ffd8d7c561ec688bb6d1c81041984
Quattro Musser <quattromusser@gmail.com>
2022-12-23 18:49:43 -0500

    after a restart, got 5 pt 2 working

--- 2015/22/05/02.py
+++ 2015/22/05/02.py
@@ -1,52 +1,57 @@
 
-data = open("test.txt").read()
+import sys
+
+data = open("input.txt").read()
 data = data.split("\n")
+
+nice_count = 0
+
 for line in data:
-    # unless...
-    # rule 1: must be xxybkxx or xxxx not xxx for some x
-    # get all unique letters
-    passed_one = False
-    passed_both = False
-    line_list = [*line]
-    char_set = set(line_list)
-    for letter in char_set:
-        # only check next letter if we havn't passed rule one yet
-        if not passed_one:
-            all_runs = {}
-            current_run = 1
-            # don't consider the last char in line
-            for i, other_letter in enumerate(line_list[:-1]):
-                if other_letter == letter:
-                    if other_letter == line_list[i + 1]:
-                        # next matches
-                        current_run += 1
-                    else:
-                        # next doesn't match
-                        if current_run > 1:
-                            # we have some run
-                            tag = str(current_run)
-                            current_run = 1
-                            if tag in all_runs.keys():
-                                all_runs[tag] += 1
-                            else:
-                                all_runs[tag] = 1
-        num_2and3s = 0
-        if all_runs.get("2"):
-            num_2and3s += all_runs["2"]
-        if all_runs.get("3"):
-            num_2and3s += all_runs["3"]
-        if num_2and3s > 1:
-            passed_one = True
-            if all_runs.get("3"):
-                # must have passed rule two as well!
-                passed_both = True
-        if all_runs.get("4"):
-            passed_one = True
-            # must have passed rule two as well!
-            passed_both = True
+    # nice strings needs both (1) and (2) to be true
+    # (1) contains a pair of any two letters that appear more than once in the string, eg: sy, sy
+    passed_1 = False
+    # for every two letter group in string...
+    for i in range(len(line) - 1):
+        # end this check if already passed
+        if passed_1:
+            break
+        pair = line[i] + line[i + 1]
+        # remove that group, split string into two substring (one or more might be empty)
+        subStrings = []
+        if (i > 1):
+            # need a starting substring
+            subStrings.append(line[:i])
+        if (i < len(line) - 3):
+            # need an ending substring
+            subStrings.append(line[i + 2:])
+        # check if the two letter group is found in some substring
+        for sub in subStrings:
+            if passed_1:
+                break
+            for ii in range(len(sub) - 1):
+                # if found, set passed_1 to true, break out of (1) for loop 
+                checkPair = sub[ii] + sub[ii + 1]
+                if (checkPair == pair):
+                    passed_1 = True
+                    break
+    if not passed_1:
+        # naughty
+        continue
 
+    # (2) contains one letter which repeats with any other letter between, eg: aya, xxx
+    passed_2 = False
+    # for every letter
+    for i in range(len(line) - 2):
+        # check if letter two after matches
+        if line[i] == line[i + 2]:
+            # if yes, passed (2)
+            passed_2 = True
+            break
 
-    # or, rule 2:
-    # for some x, need something like xyx, xax, or even xxx
-    passed_two = False
+    if not passed_2:
+        continue
+
+    nice_count += 1
+
+print(nice_count)
 
