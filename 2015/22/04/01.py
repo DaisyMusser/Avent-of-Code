6da6093bf3469696bced347dbc0afd649c6a4ad3
Quattro Musser <quattromusser@gmail.com>
2022-12-23 11:30:23 -0700

    day 4 part 1 done

---
+++ 2015/22/04/01.py
@@ -0,0 +1,15 @@
+
+import hashlib
+import sys
+
+myInput = "iwrupvqb"
+
+for i in range(0, 99999999):
+    toHash = myInput + str(i)
+    result = hashlib.md5(toHash.encode())
+    output = result.hexdigest() 
+    if (output[:5] == "00000"):
+        print(i)
+        sys.exit(0)
+print("failure")
+
