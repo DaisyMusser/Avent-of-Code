6e8abeb3e8238bf545dfb12cf48ba99b21c001af
Quattro Musser <quattromusser@gmail.com>
2022-12-24 12:54:37 -0500

    day 4 works and is fast, I was just silly

--- 2015/22/04/02.py
+++ 2015/22/04/02.py
@@ -1,14 +1,15 @@
 
 import hashlib
 import sys
+from tqdm import tqdm
 
 myInput = "iwrupvqb"
 
-for i in range(0, 99999999):
+for i in tqdm(range(0, 99999999999999999), desc="Searching..."):
     toHash = myInput + str(i)
     result = hashlib.md5(toHash.encode())
     output = result.hexdigest() 
-    if (output[:5] == "000000"):
+    if (output[:6] == "000000"):
         print(i)
         sys.exit(0)
 print("failure")
