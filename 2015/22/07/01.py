6bcdd866efaa1088b02f2ad50a125a453d41d7f5
Quattro Musser <quattromusser@gmail.com>
2023-01-25 18:31:34 -0700

    first try reading input for 01

--- 2015/22/07/01.py
+++ 2015/22/07/01.py
@@ -1,47 +1,55 @@
 
 from classy import *
+from tqdm import tqdm
 
-# test
-x0  = value(123)
-x1  = value(456)
-x2  = value(2)
-x3  = address("x")
-x4  = address("y")
-x5  = value(2)
-x6  = opperation("NOT")
-x7  = opperation("LSHIFT")
-x8  = opperation("AND")
-x9  = opperation("OR")
-x10 = opperation("RSHIFT")
-x11 = opperation("NOT")
-x12 = address("h")
-x13 = address("f")
-x14 = address("d")
-x15 = address("e")
-x16 = address("g")
-x17 = address("i")
+# given and address name, gets the address
+def get_address(new_addr_name, mem):
+    for addr in mem:
+        if addr.name == new_addr_name:
+            return addr
+    new_addr = address(new_addr_name)
+    mem.append(new_addr)
+    return new_addr
 
-x17.addParent(x11)
-x16.addParent(x10)
-x15.addParent(x9)
-x14.addParent(x8)
-x13.addParent(x7)
-x12.addParent(x6)
-x6.addParent(x3)
-x7.addParent(x3)
-x7.addParent(x2)
-x8.addParent(x3)
-x8.addParent(x4)
-x9.addParent(x3)
-x9.addParent(x4)
-x10.addParent(x4)
-x10.addParent(x5)
-x11.addParent(x4)
-x3.addParent(x0)
-x4.addParent(x1)
+# given a child, adds addr/number parent
+def add_addr_num(child, string):
+        if string.isnumeric():
+            child.addParent(value(int(string)))
+        else:
+            child.addParent(get_address(string, mem))
+    
+# list of addr objects
+mem = []
 
-circut = [x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17]
+data = open("input.txt").read()
+data = data.split("\n")
+for line in tqdm(data):
+    if line == "STOP":
+        for addr in mem:
+            if addr.name == "a":
+                print("a: " + str(addr.getValue()))
+                exit(0)
+    else:
+        spaces = line.count(" ")
+        words = line.split(" ")
+        # lines with 2 spaces are:
+        # addr/number -> addr
+        if spaces == 2:
+            child = get_address(words.pop(), mem)
+            add_addr_num(child, words[0])
+        # lines with 3 spaces are:
+        # NOT addr/number -> addr
+        elif spaces == 3:
+            child = get_address(words.pop(), mem)
+            not_oppr = opperation("NOT")
+            child.addParent(not_oppr)
+            add_addr_num(not_oppr, words[1])
+        # lines with 4 spaces are:
+        # addr/number OPPR addr/number = addr
+        elif spaces == 4:
+            child = get_address(words.pop(), mem)
+            oppr = opperation(words[1])
+            child.addParent(oppr)
+            add_addr_num(oppr, words[0])
+            add_addr_num(oppr, words[2])
 
-for n in circut:
-    if isinstance(n, address):
-        print(n.name + ": " + str(n.getValue()))
