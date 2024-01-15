0e627eaae728904150b3e92acd6f822f38a85e82
Quattro Musser <quattromusser@gmail.com>
2024-01-14 20:12:33 -0700

    part two went pretty smooth! starting from scratch on most of the core line iteration was the move. done in py.

--- 2023/03/02.py
+++ 2023/03/02.py
@@ -31,34 +31,69 @@ def getNeighborIndices(x, y_start, y_end, l) -> list[(int, int)]:
     neighbors += [(x + 1, y) for y in range(y_start - 1, y_end + 2) if inBounds((x + 1, y), l)]
     return neighbors
 
+"""
+Given the index of a digit and the line it's in, returns 
+the full number digit belongs to, and list of all indices 
+number takes up
+"""
+def getNumber(i, x, line):
+    str_num = line[i]
+    indices = [(x, i)]
+    # search right
+    j = i
+    while True:
+        j += 1
+        if re.search("[0-9]", line[j]) != None:
+            str_num += line[j]
+            indices.append((x, j))
+        else:
+            break
+    # search left
+    while True:
+        i -= 1
+        if re.search("[0-9]", line[i]) != None:
+            str_num = line[i] + str_num
+            indices.append((x, i))
+        else:
+            break
+    return int(str_num), indices
+
 
 if __name__ == "__main__":
     lines = open("input.txt").read().split("\n")
     lines.remove("")
-    total = 0 
-    for xlines, line in enumerate(lines):
-        # get all numbers in the line
-        nums = re.findall("(?<![0-9])[0-9]+(?![0-9])", line)
-        if len(nums) != len(set(nums)):
-            q = 0
-        for num in nums:
-            y_start = line.find(num)
-            y_end = y_start + len(num) - 1
-            # use getNeighborIndices to see what you have to check
-            neighbors = getNeighborIndices(xlines, y_start, y_end, len(line))
-            # check those indices for symbols.
-            for x, y in neighbors:
-                # If any found, halt, add number to total
-                if re.search("\\.|[0-9]", lines[x][y]) == None:
-                    total += int(num)
-                    break
-            # need to erase visited numbers 
-            mutableLine = list(line)
-            for y in range(y_start, y_end + 1):
-                mutableLine[y] = "."
-            line = "".join(mutableLine)
+    total = 0
+    # line: "617*......"
+    for x, line in enumerate(lines):
+        # find all the gears in line
+        gears = re.findall("\\*", line)
+        for z in gears:
+            # get first gear in line
+            g = line.find("*")
+            neighbors = getNeighborIndices(x, g, g, len(line))
+            numbers = []
+            checked = []
+            # check all neighboring indices:
+            for nx, ny in neighbors:
+                # ignore if checked
+                if (nx, ny) in checked:
+                    pass
+                else:
+                    # if its a number:
+                    if re.search("[0-9]", lines[nx][ny]) != None:
+                        # find complete number
+                        num, indices = getNumber(ny, nx, lines[nx])
+                        # add number to numbers
+                        numbers.append(num)
+                        # mark all number's indices as checked 
+                        checked += indices
+            # if exactly two numbers add gear ratio to total
+            if len(numbers) == 2:
+                gear_ratio = numbers[0] * numbers[1]
+                total += gear_ratio
+            # erase gear from line
+            mutable_line = list(line)
+            mutable_line[g] = "."
+            line = "".join(mutable_line)
     print(total)
 
-# 556822 wrong
-# 556461 is too high
-# 276860 is too low
